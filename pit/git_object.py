1069b542f1caf53443faa36b275c86f0ab463031
shidenggui <longlyshidenggui@gmail.com>
2021-10-27 21:35:09 +0800

    feat: support commit

--- pit/git_object.py
+++ pit/git_object.py
@@ -1,5 +1,6 @@
 import hashlib
 from dataclasses import dataclass, field
+from datetime import datetime
 
 
 @dataclass
@@ -25,7 +26,31 @@ class Blob(GitObject):
 
 @dataclass()
 class Commit(GitObject):
-    type: str = "commit"
+    tree_hash: str
+    timestamp: int
+    timezone: str  # +0800
+    user_name: str
+    user_email: str
+    message: str
+
+    def __post_init__(self):
+        self.type = "commit"
+
+        # b'commit 188\x00tree 2e81171448eb9f2ee3821e3d447aa6b2fe3ddba1\nauthor shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\ncommitter shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\n\nadd a.txt\n'
+        content = b"tree %s\nauthor %s <%s> %d %s\ncommitter %s <%s> %d %s\n\n%s\n" % (
+            self.tree_hash.encode(),
+            self.user_name.encode(),
+            self.user_email.encode(),
+            self.timestamp,
+            self.timezone.encode(),
+            self.user_name.encode(),
+            self.user_email.encode(),
+            self.timestamp,
+            self.timezone.encode(),
+            self.message.encode(),
+        )
+        self.saved = b"commit %d\x00%s" % (len(content), content)
+        self.hash = hashlib.sha1(self.saved).hexdigest()
 
 
 @dataclass()
@@ -70,4 +95,19 @@ if __name__ == "__main__":
     assert (
         a_tree.saved
         == b"tree 33\x00100644 a.txt\x00\xce\x016%\x03\x0b\xa8\xdb\xa9\x06\xf7V\x96\x7f\x9e\x9c\xa3\x94FJ"
+    ), a_tree.saved
+
+    print("Test Commit")
+    a_commit = Commit(
+        tree_hash=a_tree.hash,
+        timestamp=1635305754,
+        timezone="+0800",
+        message="add a.txt",
+        user_name="shidenggui",
+        user_email="longlyshidenggui@gmail.com",
+    )
+    print(a_commit)
+    assert (
+        a_commit.saved
+        == b"commit 188\x00tree 2e81171448eb9f2ee3821e3d447aa6b2fe3ddba1\nauthor shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\ncommitter shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\n\nadd a.txt\n"
     ), a_tree.saved
