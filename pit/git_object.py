617e672685603688cee32ae43569d927cd8e7859
shidenggui <longlyshidenggui@gmail.com>
2021-10-28 20:40:29 +0800

    feat: rename hash to oid

--- pit/git_object.py
+++ pit/git_object.py
@@ -6,8 +6,8 @@ from datetime import datetime
 @dataclass
 class GitObject:
     type: str = field(init=False)
+    oid: str = field(init=False)
     saved: bytes = field(init=False)
-    hash: str = field(init=False)
 
 
 @dataclass()
@@ -21,42 +21,43 @@ class Blob(GitObject):
             len(self.content),
             self.content,
         )
-        self.hash = hashlib.sha1(self.saved).hexdigest()
+        self.oid = hashlib.sha1(self.saved).hexdigest()
 
 
 @dataclass()
 class Commit(GitObject):
-    tree_hash: str
+    tree_oid: str
     timestamp: int
     timezone: str  # +0800
     user_name: str
     user_email: str
     message: str
+    parent_oid: str = None
 
     def __post_init__(self):
         self.type = "commit"
 
         # b'commit 188\x00tree 2e81171448eb9f2ee3821e3d447aa6b2fe3ddba1\nauthor shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\ncommitter shidenggui <longlyshidenggui@gmail.com> 1635305754 +0800\n\nadd a.txt\n'
-        content = b"tree %s\nauthor %s <%s> %d %s\ncommitter %s <%s> %d %s\n\n%s\n" % (
-            self.tree_hash.encode(),
+        user_info = b"%s <%s> %d %s" % (
             self.user_name.encode(),
             self.user_email.encode(),
             self.timestamp,
             self.timezone.encode(),
-            self.user_name.encode(),
-            self.user_email.encode(),
-            self.timestamp,
-            self.timezone.encode(),
-            self.message.encode(),
         )
+        contents = [b"tree %s" % self.tree_oid.encode()]
+        if self.parent_oid:
+            contents.append(b"parent %s" % self.parent_oid.encode())
+        contents.append(b"author %s\ncommitter %s" % (user_info, user_info))
+        contents.append(b"\n%s\n" % self.message.encode())
+        content = b"\n".join(contents)
         self.saved = b"commit %d\x00%s" % (len(content), content)
-        self.hash = hashlib.sha1(self.saved).hexdigest()
+        self.oid = hashlib.sha1(self.saved).hexdigest()
 
 
 @dataclass()
 class Entry:
+    oid: str
     name: str
-    hash: str
     mode: bytes = field(default=b"100644")
     saved: bytes = field(init=False)
 
@@ -64,14 +65,13 @@ class Entry:
         self.saved = b"%s %s\x00%s" % (
             self.mode,
             self.name.encode(),
-            bytes.fromhex(self.hash),
+            bytes.fromhex(self.oid),
         )
 
 
 @dataclass()
 class Tree(GitObject):
     entries: list[Entry]
-    type: str = field(init=False)
 
     def __post_init__(self):
         self.type = "tree"
@@ -80,7 +80,7 @@ class Tree(GitObject):
         content = b"".join(contents)
         self.saved = b"tree %d\x00%s" % (len(content), content)
 
-        self.hash = hashlib.sha1(self.saved).hexdigest()
+        self.oid = hashlib.sha1(self.saved).hexdigest()
 
 
 if __name__ == "__main__":
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     assert a_txt.saved == b"blob 6\x00hello\n"
 
     print("Test Tree")
-    a_tree = Tree(entries=[Entry(name="a.txt", hash=a_txt.hash)])
+    a_tree = Tree(entries=[Entry(name="a.txt", oid=a_txt.oid)])
     print(a_tree)
     assert (
         a_tree.saved
@@ -99,7 +99,7 @@ if __name__ == "__main__":
 
     print("Test Commit")
     a_commit = Commit(
-        tree_hash=a_tree.hash,
+        tree_oid=a_tree.oid,
         timestamp=1635305754,
         timezone="+0800",
         message="add a.txt",
