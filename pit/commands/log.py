1f717b8a2cda8d920d36294cd0e84ea908b53b92
shidenggui <longlyshidenggui@gmail.com>
2021-11-26 08:45:43 +0800

    opt: refactor

--- pit/commands/log.py
+++ pit/commands/log.py
@@ -1,5 +1,6 @@
 import datetime
 from collections import defaultdict
+from functools import cached_property
 
 from pit.commands.base import BaseCommand
 from pit.constants import Color
@@ -12,32 +13,46 @@ class LogCommand(BaseCommand):
         super().__init__(root_dir)
         self.oneline = oneline
 
+    @cached_property
+    def head(self):
+        return self.repo.refs.read_head()
+
+    @cached_property
+    def oid2branches(self) -> dict[str, list[str]]:
+        oid2branch = defaultdict(list)
+
+        for branch in self.repo.refs.list_branches():
+            oid2branch[self.repo.refs.read_branch(branch)].append(branch)
+        return oid2branch
+
     def run(self):
-        head = self.repo.refs.read_head()
-        if not head:
+        if not self.head:
             return
 
-        head = self.repo.refs.read_head()
-        oid2branches = self._get_oid2branches()
-        for commit in self._find_parent(head):
-            decoration = ""
-            if branches := oid2branches.get(commit.oid):
-                decoration = self._generate_decoration(
-                    branches, is_head=head == commit.oid
-                )
-            if self.oneline:
-                self._show_oneline(commit, decoration)
-            else:
-                self._show_medium(commit, decoration)
+        for commit in self._find_parent(self.head):
+            self._show_commit(commit)
 
-    def _generate_decoration(self, branches: list[str], *, is_head: str) -> str:
+    def _show_commit(self, commit: Commit):
+        decoration = self._generate_decoration(commit.oid)
+        if self.oneline:
+            self._show_oneline(commit, decoration)
+        else:
+            self._show_medium(commit, decoration)
+
+    def _generate_decoration(self, oid: str) -> str:
+        branches = self.oid2branches.get(oid)
+        if not branches:
+            return ""
+
         coloreds = []
         for branch in branches:
             colored = f"{Color.GREEN}{Color.BOLD}{branch}{Color.RESET_ALL}"
             coloreds.append(colored)
-        joined_branches = ', '.join(coloreds)
-        if is_head:
-            joined_branches = f"{Color.CYAN}{Color.BOLD}HEAD -> {Color.RESET_ALL}{joined_branches}"
+        joined_branches = ", ".join(coloreds)
+        if self.head == oid:
+            joined_branches = (
+                f"{Color.CYAN}{Color.BOLD}HEAD -> {Color.RESET_ALL}{joined_branches}"
+            )
         return f"{Color.YELLOW}({Color.RESET_ALL}{joined_branches}{Color.YELLOW}){Color.RESET_ALL}"
 
     def _show_medium(self, commit: Commit, decoration: str):
@@ -61,9 +76,3 @@ Date:   {datetime.datetime.fromtimestamp(commit.author.timestamp).strftime("%a %
         yield commit
         if commit.parent_oid:
             yield from self._find_parent(commit.parent_oid)
-
-    def _get_oid2branches(self) -> dict[str, list[str]]:
-        oid2branch = defaultdict(list)
-        for branch in self.repo.refs.list_branches():
-            oid2branch[self.repo.refs.read_branch(branch)].append(branch)
-        return oid2branch
